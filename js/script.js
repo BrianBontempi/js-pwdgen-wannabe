ab2c6082b00e205cc4f3cae69f37842e3a69e138
BrianBontempi <brian.bont54@gmail.com>
2023-12-16 14:04:40 +0100

    ask name and add variable

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ console.log("JS OK")
 
 const elemento = document.getElementById("password")
 console.log(elemento)
+
+// ! chiedo il nome
+
+const firstName = prompt("Come ti chiami?")
+console.log(firstName)
